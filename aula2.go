93f63f231aa68294e3df1cb855d9358230a21ef5
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-30 10:14:04 -0300

    aula 2, operadores curtos

---
+++ aula2.go
@@ -0,0 +1,20 @@
+// aula 2, operador curto de demonstração
+
+package main
+
+
+import (
+
+	"fmt"
+
+)
+
+func main(){
+
+	x := 10.0
+	y := "olá bom dia"
+
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("y: %v, %T", y, y)
+
+}
