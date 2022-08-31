9e50e22cfdbf072eacf066cef840084c9aa52c8e
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-31 15:58:11 -0300

    atribuindo valor zero

---
+++ aula10-exercicio2.go
@@ -0,0 +1,22 @@
+package main
+
+
+import (
+
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main(){
+
+	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+
+
+
+}
+
+
+// atribui o valor zero quando você não inicializa uma vairável
