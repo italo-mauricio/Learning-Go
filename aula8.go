b4e339d5787bc29b8423df5d255de201185ee9bc
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-31 14:23:23 -0300

    trabalhando com conversão de tipos

--- aula8.go
+++ aula8.go
@@ -8,14 +8,29 @@ import (
 	"fmt"
 
 )
-
+type hotdog int
+var b hotdog = 102
 
 func main() {
 
-
-
-
+	x := 10
+	fmt.Printf("%v\n", x)
+	
+	x = int (b)
+	fmt.Printf("%v\n", x)
 
 }
 
 
+/*
+	Conversão, não coerção:
+		Conversão de tipos é o que soa.
+		Em Go não se diz casting, se diz conversion.
+		a = int(b)
+		ref/spec#Conversions
+		Fim da sessão. Parabéns! Dicas, motivação e exercícios.
+
+
+		by: https://github.com/vkorbes/aprendago/blob/master/OUTLINE.md
+
+*/
